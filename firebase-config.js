405863e0d2aa9480bb04d50adb728a7b1948c059
Owner <1001991146@edu-haifa.org.il>
2026-06-13 08:06:23 +0300

    Disable Firebase and set application to default to local storage sync mode

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
-// Fill in your Firebase project configuration here:
+// Fill in your Firebase project configuration here (leave empty to run locally without Firebase):
 const firebaseConfig = {
-  apiKey: "AIzaSyCEwlTp4Yo2boARCieNTITIT0MkMNZiOv0",
-  authDomain: "king-of-the-beach-queue.firebaseapp.com",
-  projectId: "king-of-the-beach-queue",
-  storageBucket: "king-of-the-beach-queue.firebasestorage.app",
-  messagingSenderId: "611422584946",
-  appId: "1:611422584946:web:ddb42c1d933937090c82eb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Export for browser accessibility
